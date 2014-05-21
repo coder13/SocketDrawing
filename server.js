38f94082dcefdd741f19e4738fa75132feea6e94
coder13 <choover11@gmail.com>
2014-05-21 06:17:38 -0700

    changes

--- server.js
+++ server.js
@@ -11,50 +11,15 @@ var server = http.createServer(function(req, res) {
     console.log('Listening at: http://localhost:' + port);
 });
 
-socketIO.listen(server).on('connection', function (socket) {
-    socket.on('message', function (msg) {
-        socket.broadcast.emit('message', msg);
+var data = "";
+
+socketIO.listen(server).on('connection', function (client) {
+	console.log('client connected');
+	client.emit('message', data);
+
+    client.on('message', function (msg) {
+    	data = msg;
+        client.broadcast.emit('message', msg);
     });
+
 }).set('log level', 1);
-
-// var io = require('socket.io').listen(80);
-// var Hapi = require('hapi'), port = 8000;
-
-
-// io.sockets.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-
-// var server = new Hapi.Server('localhost', port);
-
-// function route(request, reply) {
-// 	reply.view('index');
-// }
-
-// server.route({ method: 'GET', path: '/', handler:route});
-
-// server.route({
-//   method: 'GET',
-//   path: '/{path*}',
-//   handler: {
-//     directory: {
-//       path: './',
-//       listing: false,
-//       index: true
-//     }
-//   }
-// });
-
-// server.views({
-//     engines: {
-//         jade: 'jade'
-//     },
-//     path: './templates'
-// });
-
-// server.start(function () {
-//     console.log("server running on port " + port.toString());
-// });
