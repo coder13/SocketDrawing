ca41b94869f0194f790f6203fe0ff4b3def5af58
coder13 <choover11@gmail.com>
2014-06-03 13:39:57 -0700

    changes

--- js/client.js
+++ js/client.js
@@ -1,4 +1,15 @@
+var path = "M ";
+
 $(function() {
+
+    var svgCon = $("#mainSvg");
+    svgCon.on("click", function(event) {
+        path += event.offsetX + "," + event.offsetY + " ";
+
+        document.getElementById('path').setAttribute("d", path);
+    });
+
+
     var iosocket = io.connect();
 
     iosocket.on('connect', function () {
@@ -8,11 +19,8 @@ $(function() {
     iosocket.on('message', function(message) {
         $('#textArea').val(message);
     });
+    
     iosocket.on('disconnect', function() {
         $('#status').text('Status: Disconnected');
-    });
-
-    $('#textArea').on("input propertychange", function(event) {
-        iosocket.send($('#textArea').val());   
     });
 });
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var fs = require('fs'), 
 	http = require('http'),	
 	socketIO = require('socket.io'), 
-	port = 8080;
+	port = 8000;
  
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end(fs.readFileSync(__dirname + '/index.html'));
+    try {
+        if (req.url == '/') {
+            res.writeHead(200, { 'Content-type': 'text/html'});
+            res.end(fs.readFileSync(__dirname + '/index.html'));
+        }
+        
+        if (req.url.split('/')[1] == 'js')
+            res.writeHead(200, { 'Content-type': 'text/javascript'});
+        else if (req.url.split('/')[1] == '')
+            res.writeHead(200, { 'Content-type': 'text/plain'});
+        
+        res.end(fs.readFileSync(__dirname + req.url));
+    } catch (e) {}
 }).listen(port, function() {
     console.log('Listening at: http://localhost:' + port);
 });
 
-var data = "";
+var data = "M ";
 
 socketIO.listen(server).on('connection', function (client) {
 	console.log('client connected');
